e6acba1bea4d672debae546d1e8a8dd83cb0d94e
aigerim-aidilova <52512970+aigerim-aidilova@users.noreply.github.com>
2019-07-19 13:25:19 -0500

    Merge branch 'master' of https://github.com/BrkyOncl/StarBugz
    # Conflicts:
    #	src/test/java/Pages/LoginPage.java

--- src/test/java/Project/Aigerim.java
+++ src/test/java/Project/Aigerim.java
@@ -20,12 +20,20 @@ public class Aigerim {
         HomePage hp = new HomePage();
         Thread.sleep(2000);
         Assert.assertTrue(hp.logo.isDisplayed());
+        Assert.assertTrue(hp.footer.isDisplayed());
         Assert.assertTrue(hp.calendarButton.isDisplayed()&&hp.calendarButton.isEnabled());
         Assert.assertTrue(hp.listButton.isDisplayed()&&hp.listButton.isEnabled());
         Assert.assertTrue(hp.pivotButton.isDisplayed()&&hp.pivotButton.isEnabled());
         Assert.assertTrue(hp.previousButton.isDisplayed()&&hp.previousButton.isEnabled());
         Assert.assertTrue(hp.nextButton.isDisplayed()&&hp.nextButton.isEnabled());
+
+        String expected = "1-80";
+        Assert.assertEquals(hp.currentPageInfo.getText(),expected);
         hp.nextButton.click();
+        String expectedAfterClicking = "81-121";
+        Assert.assertEquals(hp.currentPageInfo.getText(),expectedAfterClicking);
+
+
 
 
 
