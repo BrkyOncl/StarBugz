83784c498e7f15efd8f82059bc7b3ae8d2ee94e3
aigerim-aidilova <52512970+aigerim-aidilova@users.noreply.github.com>
2019-08-07 15:23:09 -0500

    Merge branch 'master' of https://github.com/BrkyOncl/StarBugz
    # Conflicts:
    #	src/test/java/Project/Aigerim.java

--- src/test/java/Runners/Runner.java
+++ src/test/java/Runners/Runner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = "html:target/cucumberreports",
-        features = "src/test/resources/features",
+        features = "src/test/resourses/features",
         glue = "steps",
         tags = "@Brite",
         dryRun = true
